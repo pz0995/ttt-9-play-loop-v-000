c22f2d06386406894016b8785a6333175e01e565
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-12 03:24:35 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,10 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  amount_of_turns = 0
+  while amount_of_turns < 9
+    turn(board)
+    amount_of_turns = amount_of_turns + 1
+  end
+end
